c7c9f8869e16f2187cd9d02a868944cf38c2d3f5
shivam singh rajput <shivamjsingh1205@gmail.com>
2024-12-21 16:48:16 +0530

    add some css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,27 +1,26 @@
 import { images } from '../assets/image';
+
 const NavBar = () => {
   return (
-    <section className="relative bg-gray-200 min-h-screen flex items-center justify-center ">
-      {/* <div
-        className="absolute inset-0 bg-cover bg-center"
-        style={{
-          backgroundImage:
-            "url('https://ld-wp73.template-help.com/wordpress/prod_11365/v1/wp-content/uploads/2021/05/home-1.jpg')",
-          
-        }}
-      ></div> */}
-      <img src={images.BgImage} alt="" className="absolute inset-0 bg-cover bg-center h-full w-full" />
-      <div className="relative container mx-auto px-6 flex flex-col md:flex-row items-center">
+    <section className="relative bg-gray-200 min-h-screen flex items-center justify-center">
+      {/* Background Image */}
+      <img
+        src={images.BgImage}
+        alt="Background"
+        className="absolute inset-0 object-cover h-full w-full"
+      />
+
+      {/* Content Container */}
+      <div className="relative container mx-auto px-4 sm:px-6 lg:px-8 flex flex-col md:flex-row items-center justify-between">
         {/* Text Content */}
-        <div className="md:w-1/2 text-white z-10">
-          <h1 className="text-4xl md:text-5xl font-bold mb-4 ">
+        <div className="text-center md:text-left md:w-1/2 z-10">
+          <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-4 text-white">
             Complex Approach{" "}
-            <span className="text-orange">to Pest Control</span>
+            <span className="text-orange-500">to Pest Control</span>
           </h1>
-          <p className="text-lg text-white mb-6">
+          <p className="text-base sm:text-lg text-gray-100 mb-6">
             We are here to resolve the most complex issue!
           </p>
-          
         </div>
       </div>
     </section>
